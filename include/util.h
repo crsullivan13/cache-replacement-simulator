abc86b438063d5213707eb80d2d44cd44720103e
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2025-06-20 09:39:05 -0500

    Make util static header

--- include/util.h
+++ include/util.h
@@ -2,10 +2,42 @@
 #define UTIL_H
 
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 
-bool isValidConfig(const char* config);
-int findPowerOf2(int value);
-uint64_t* gen_rand_addresses(int number_of_addresses);
+static bool isValidConfig(const char* config) {
+    if ( config == NULL ) {
+        fprintf(stderr, "ERROR: Config was NULL\n");
+        return false;
+    } else {
+        return  ( strncmp("random", config, strlen("random")) == 0 ||
+            strncmp("lru", config, strlen("lru")) == 0 ||
+            strncmp("plru", config, strlen("plru")) == 0 );
+    }
+}
+
+// non-powers of 2 will waste address bits
+static int findPowerOf2(int value) {
+    int power = 0;
+    
+    while ( ( 1 << power ) < value ) {
+        power += 1;
+    }
+
+    return power;
+}
+
+// must call free on returned list
+static uint64_t* gen_rand_addresses(int number_of_addresses) {
+    uint64_t* addresses = malloc(sizeof(uint64_t) * number_of_addresses);
+
+    for ( int i = 0; i < number_of_addresses; i++) {
+        addresses[i] = rand();
+    }
+
+    return addresses;
+}
 
 #endif
